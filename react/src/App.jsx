01cd532bf6ed77a4f6449b487e83bd85affbd2db
uttamongithb <uttamkumar9234567@gmail.com>
2026-02-04 16:12:40 +0530

    hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,30 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Fetchapi from './components/Fetchapi'
 
 const App = () => {
-const [count, setCount] = useState(0);
-
-const plus = () =>{
-  setCount(count+1);
-}
-const minus = () => {
-  setCount(count-1)
-}
-
-const reset = () =>{
-  setCount(0)
-}
-
   return (
     <>
-    <div>
-      {count}
-    </div>
-
-    <div onClick={plus}>
-      +
-    </div >
-    <div onClick={minus}>-</div>
-    <div onClick={reset}>reset</div>
+      <Fetchapi/>
+    
     </>
   )
 }
