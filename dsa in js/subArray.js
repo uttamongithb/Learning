0540f23bd018e54a371fe25894d62eb383c80c84
Your Name <uttamkumar9234567@gmail.com>
2026-02-10 16:00:38 +0530

    updated

--- dsa in js/subArray.js
+++ dsa in js/subArray.js
@@ -2,12 +2,12 @@ let arr = [2, 5, 6, 3, 9, 4, 8];
 
 let maxSum = 0;
 
-for(let i=0; i<arr.length-2; i++){
-let arrSum = arr[i]+arr[i+1]+arr[i+2];
+for (let i = 0; i < arr.length - 2; i++) {
+    let arrSum = arr[i] + arr[i + 1] + arr[i + 2];
 
-if(arrSum > maxSum){
-    maxSum = arrSum;
-}
+    if (arrSum > maxSum) {
+        maxSum = arrSum;
+    }
 
 }
 
