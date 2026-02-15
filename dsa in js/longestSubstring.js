72aca0268fc85f79363a1565ce02deae92daddc9
Your Name <uttamkumar9234567@gmail.com>
2026-02-15 23:00:09 +0530

    still i do not resolved it

--- dsa in js/longestSubstring.js
+++ dsa in js/longestSubstring.js
@@ -1,7 +1,21 @@
-let s = "abcabcbb";
+let s = "dvdf";
+
+
 
 let str = '';
 
+let mx = 0;
+
+
 for(let i=0; i<s.length; i++){
-    
+    if(!str.includes(s[i])){
+        str += s[i];
+        if(str.length > mx){
+            mx++;
+        }
+    }else{
+        str = '';
+    }
 }
+
+console.log(mx);
