2408db219052ff81a9138a5bb698fe87e0958d01
Your Name <uttamkumar9234567@gmail.com>
2026-02-28 10:46:32 +0530

    i have fetched all the saved posts

--- backend by scs/src/app.js
+++ backend by scs/src/app.js
@@ -27,4 +27,14 @@ app.post('/create-post', upload.single('image'), async (req, res) => {
 });
 
 
+app.get('/posts', async (req, res) =>{
+    const posts = await postModel.find();
+
+    return res.status(200).json({
+        message: 'post fetched successfully',
+        posts
+    })
+})
+
+
 export default app;
