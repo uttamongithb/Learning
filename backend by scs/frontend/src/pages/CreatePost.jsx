1e4b85d9c0c3a71fbeb69fd0f38a78cfeb64bab4
Your Name <uttamkumar9234567@gmail.com>
2026-03-01 14:16:32 +0530

    create feed

--- backend by scs/frontend/src/pages/CreatePost.jsx
+++ backend by scs/frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import axios from 'axios';
 
 const CreatePost = () => {
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+
+    const formData = new FormData(e.target);
+
+    axios.post('http://localhost:3000/create-post', formData)
+    .then((res))
+
+  }
+
+
+
   return (
     <section className='create-post-section'>
         <h1>Create post</h1>
